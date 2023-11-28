15b64ff66d6cfc6eb83179d7dab45fc775668fb9
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-11-28 22:40:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If require
+const button=document.getElementById("enterbtn");
+const first=document.getElementById("status");
+button.addEventListener("click" ,()=>{
+	button.remove();
+alert(first.innerText);
+	
+});
